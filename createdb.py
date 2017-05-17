81a5ce9baee13e0c300c1d4055e0053be0ff0265
riow1983 <ryoiryojoho@gmail.com>
2017-05-18 06:35:44 +0900

    deleted createdb.py

--- createdb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE report (personnel TEXT, starttime DATETIME, endtime DATETIME, client TEXT, section TEXT, patient TEXT, country TEXT, summary TEXT)')
-print("Table created successfully")
-conn.close()
